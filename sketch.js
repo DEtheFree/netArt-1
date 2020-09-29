5ca0b069d5bd3843ff1cd6f6f7406681241286d2
Jnguyen7577 <jnguyen7577@yahoo.com>
2020-09-29 01:06:31 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// randomizer
+
+let dogs = ["mario", "luigi", "waluigi", "wario", "peach", "toad"];
+
+function setup() {
+	createCanvas(600, 600);
+	background(200);
+
+	console.log(dogs[0]);
+
+
+}
+
+function draw(){
+
+}
