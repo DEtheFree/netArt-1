24c70fdb0a22cac1097c91051b2c7db0eeceda05
Jnguyen7577 <jnguyen7577@yahoo.com>
2020-09-30 20:58:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,21 +21,53 @@ let dogs = [{
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
 	createCanvas(600, 600);
 	background(200);
+	textSize(32);
+
+	text("click to randomize", 50, 50);
+
 
 
 }
 
 function draw(){
 
+	if (animating == true){
+		ellipse(random(width), random(height), random(50, 200));
+
+	}
+
+}
+
+function randomizer(){
+	animating = false;
+
+	if (dogs[0]){
+
+	background(random(200, 255));
+	randomIndex = int(random(dogs.length));
+	text(dogs[randomIndex].name + "'s favorite color is " + 
+		dogs[randomIndex].color, 50, 50);
+	dogs.splice(randomIndex, 1);
+} else {
+	background(random(200, 255));
+	text("nothing left", 50, 50);
+	}
+
 }
 
 function mousePressed(){
-	background(random(200, 255));
-	randomIndex = int(random(dogs.length));
-	text(dogs[randomIndex].name, 50, 50);
-	dogs.splice(randomIndex, 1);
+	animating = true;
+	setTimeout(randomizer, 2000);
+
+
+
 }
+
+
+
+
